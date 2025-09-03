04d89911424cd532af2e8f2891d524cc9178246d
TanujairamV <tanujairam.v@gmail.com>
2025-09-03 21:39:57 +0530

    sunkari

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {
-      colors: {
-        deepBlue: '#222C92',
-        softPurple: '#8536A9',
-        teal: '#1DB3A6',
-      },
-      backgroundImage: theme => ({
-        'gradient-blue-violet': 'linear-gradient(90deg, #222C92 0%, #1DB3A6 100%)',
-        'gradient-sunrise': 'linear-gradient(90deg, #FF6F61 0%, #D5006D 100%)',
-      }),
-      fontFamily: {
-        sans: ['Helvetica Neue', 'Arial', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
